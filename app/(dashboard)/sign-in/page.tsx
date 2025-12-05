0ce09bfb549014f6c8f9cff3590b399f8d648f33
Levi Indrajaya Sutantio <alevindrajaya@gmail.com>
2025-12-06 00:03:05 +0700

    feat: create SignInPage with ConsultationForm and Footer components

---
+++ app/(dashboard)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import { ConsultationForm } from "@/components/aspire/ConsultationForm/ConsultationForm";
+import { Footer } from "@/components/aspire/Footer";
+
+export default function SignInPage() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen py-12 px-4">
+        <ConsultationForm />
+      </div>
+      <Footer />
+    </>
+  );
+}
